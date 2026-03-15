27efec0f63d97c7840f68c666665caf6c37777bb
Blazzzeee <legendgamer1007@gmail.com>
2026-03-15 23:46:12 +0530

    add strict FSM for transitions

--- job/types.go
+++ job/types.go
@@ -2,6 +2,7 @@ package job
 
 import (
 	"context"
+	"fmt"
 	"time"
 )
 
@@ -10,19 +11,65 @@ type ID string
 type JobState int
 
 const (
-	StateQueued JobState = iota
+	StateCreated JobState = iota
+	StateQueued
 	StateRunning
 	StateSuccess
 	StateCancelled
-	StateFailed
 	StateTimeout
+	StateFailed
 )
+
+var allowedTransitions = map[JobState]map[JobState]struct{}{
+	StateCreated: {
+		StateQueued: {},
+		StateFailed: {},
+	},
+	StateQueued: {
+		StateRunning:   {},
+		StateFailed:    {},
+		StateCancelled: {},
+	},
+	StateRunning: {
+		StateSuccess: {},
+		StateTimeout: {},
+		StateFailed:  {},
+	},
+	StateSuccess:   {},
+	StateCancelled: {},
+	StateTimeout:   {},
+	StateFailed:    {},
+}
+
+func (j *Job) Transition(to JobState) error {
+	now := time.Now()
+
+	next := allowedTransitions[j.State]
+
+	// if the requested transition can be done after 'next'
+	if _, ok := next[to]; !ok {
+		return fmt.Errorf("invalid transition %v to %v", j.State, to)
+	}
+
+	switch to {
+	case StateQueued:
+		j.TimeStamps.QueuedAt = &now
+	case StateRunning:
+		j.TimeStamps.StartedAt = &now
+	case StateSuccess, StateFailed, StateCancelled, StateTimeout:
+		j.TimeStamps.FinishedAt = &now
+	}
+	j.State = to
+
+	return nil
+}
 
 // TODO: enforce strict FSM transition
 // func (j *Job) Transition(to JobState) error
 
 type JobTimeInfo struct {
 	CreatedAt  time.Time
+	QueuedAt   *time.Time
 	StartedAt  *time.Time
 	FinishedAt *time.Time
 }
@@ -48,12 +95,13 @@ type Job struct {
 	Error    error
 }
 
+// TOOD: move this to consumer
 // This component is responsible for decision making
 type Runner interface {
 	Submit(ctx context.Context, cmd []string, options Options) (ID, error)
-	Get(ctx context.Context, id ID) (*Job, error)
+	Get(id ID) (*Job, error)
 	Cancel(ctx context.Context, id ID) error
-	List(ctx context.Context) ([]*Job, error)
+	List(ctx context.Context) []*Job
 	Shutdown(ctx context.Context) error
 }
 
@@ -62,16 +110,4 @@ type Runner interface {
 type Executor interface {
 	// gives back process id that will form an association with the job itself
 	Dispatch(ctx context.Context, job *Job) <-chan WorkerResult
-}
-
-type Store interface {
-	Get(id ID) (*Job, error)
-	Push(job *Job) error
-	Pop(id ID) (*Job, error)
-}
-
-type runner struct {
-	store  Store
-	queue  Queue
-	worker WorkerPool
 }
